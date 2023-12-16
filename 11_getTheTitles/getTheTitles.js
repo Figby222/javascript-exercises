5dff9ca3f1791d0b9d30a227091c41c1b8e4e5ae
Figby222 <figby222@outlook.com>
2023-12-16 06:25:45 -0800

    Complete JavaScript exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titlesList = books.reduce((titles, book) => {
+        titles.push(book.title);
+        return titles;
+    }, [])
+    return titlesList;
 };
 
 // Do not edit below this line
