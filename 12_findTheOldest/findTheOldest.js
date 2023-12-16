571d40bede06fa9f323a83b67a8bf75b50752789
Figby222 <figby222@outlook.com>
2023-12-16 06:53:28 -0800

    Complete JavaScript exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldestPerson = people.reduce((currentOldest, newPerson, index) => {
+        if (`yearOfDeath` in newPerson == false) {
+            // they're still living
+            const date = new Date();
+            newPerson.yearOfDeath = date.getFullYear();
+        }
+        let currentOldestAge = currentOldest.yearOfDeath - currentOldest.yearOfBirth;
+        let newPersonAge = newPerson.yearOfDeath - newPerson.yearOfBirth;
 
+        if (currentOldestAge > newPersonAge) {
+            return currentOldest;
+        }
+
+        return newPerson;
+     }, {})
+    
+    return oldestPerson
 };
 
 // Do not edit below this line
