36ba3231551b4184643807c3dda55b6b27bc9163
Figby222 <figby222@outlook.com>
2023-12-16 06:15:25 -0800

    Complete JavaScript exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    index = parseInt(index);
+    if (index < 0) {
+        return `OOPS`;
+    }
+    let fibonacci = [1, 1];
+    for (let i = 1; i < index; i++) {
+        fibonacci.push(fibonacci[i-1] + fibonacci[i]);
+    }
+    return fibonacci[index-1];
 };
 
 // Do not edit below this line
