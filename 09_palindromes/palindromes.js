762f8e0552705e6fd1f276f8fd3fddda9797e707
Figby222 <figby222@outlook.com>
2023-12-16 06:04:10 -0800

    Complete JavaScript exercise 02

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g,"");
+    let stringBackwards = ``;
+    for (let i = string.length-1; i >= 0; i--) {
+        stringBackwards += string[i];
+    }
+    if (stringBackwards == string) {
+        return true;
+    }
 
+    return false;
 };
 
 // Do not edit below this line
